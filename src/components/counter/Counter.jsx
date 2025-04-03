95c888ec44a156459ed3ab0ef8a97c61991c117c
Drynkin Sergey <sergsmile29@gmail.com>
2025-04-03 10:11:20 +0300

    events

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,14 +1,32 @@
 import s from './Counter.module.css';
 
 export const Counter = () => {
+  const handleMinusClick = () => {
+    console.log('Minus click!');
+  };
+  const handlePlusClick = (value, e) => {
+    console.log(`Click  ${value} times`);
+    console.log(e);
+  };
+  const handleResetClick = event => {
+    console.log(event);
+    event.target.innerHTML = 'Hello';
+  };
+
   return (
     <div className={s.flexContainer}>
       <div className={s.wrapper}>
         <h1>{1}</h1>
         <div className={s.flex}>
-          <button className='btn'>minus</button>
-          <button className='btn'>reset</button>
-          <button className='btn'>plus</button>
+          <button onClick={handleMinusClick} className='btn'>
+            minus
+          </button>
+          <button onClick={handleResetClick} className='btn'>
+            reset
+          </button>
+          <button onClick={() => handlePlusClick(40)} className='btn'>
+            plus
+          </button>
         </div>
       </div>
     </div>
