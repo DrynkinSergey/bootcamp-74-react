2b4f6dd7d84591a3d0e0936f7003366250a29c22
Drynkin Sergey <sergsmile29@gmail.com>
2025-04-09 17:35:19 +0300

    init m4l1

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,8 @@
-const Header = ({ logout }) => {
+const Header = () => {
   return (
     <header>
-      <h2>Forms</h2>
-      <button onClick={logout}>Logout</button>
+      <h2>Http, Axios</h2>
+      <h2>Module 4 Part 1</h2>
     </header>
   );
 };
--- src/components/posts/SearchBar.jsx
+++
@@ -1,8 +0,0 @@
-const SearchBar = ({ handleChangeSearch }) => {
-  return (
-    <div>
-      <input onChange={handleChangeSearch} placeholder='Enter post title or body....' />
-    </div>
-  );
-};
-export default SearchBar;
